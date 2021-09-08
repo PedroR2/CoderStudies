d60ba1f0084f8393989be9f0ed7a2464ef0140cd
PedroR2 <pedro_rocha123@hotmail.com>
2021-09-08 11:24:05 -0300

    estudos

---
+++ Cod3r/funcoes/callable_packnomeado.py
@@ -0,0 +1,14 @@
+def calc_prec_fina(pbruto, impost, **params):
+    return pbruto+(pbruto*impost(**params))
+
+def impost1(importado):
+    return 0.22 if importado else 0.13
+
+def impost2(explosivo, nivel=1):
+    return 0.11*nivel if explosivo else 0
+
+if __name__=='__main__':
+    pbruto = 134.98
+    pfinal = calc_prec_fina(pbruto, impost1, importado=True)
+    pfinal2 = calc_prec_fina(pfinal, impost2, explosivo=True, nivel=1.5)
+    print('Preco final = {:.2f}'.format(pfinal2))
